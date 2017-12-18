9a6a93b6a3f9c34e1fc8c14a44b47903cc94946f
Antonio Guillen <antonio.thepirateking@gmail.com>
2017-12-17 22:19:07 -0600

    updated doxygen
    now has the .py files

--- html/html/search/all_6.js
+++ html/html/search/all_6.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['types',['types',['../class_web_content_1_1classes_1_1types.html',1,'WebContent::classes']]]
+  ['find_5fquantity',['find_quantity',['../class_web_content_1_1classes_1_1allocation__table.html#ad9d19b4d3ee1752e1d450e50eb34e117',1,'WebContent::classes::allocation_table']]]
 ];
